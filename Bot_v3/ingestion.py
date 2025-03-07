ad208a5b9d348ad82d794e84ebece0bcdc1395ce
incri <paudelbaibhav0@gmail.com>
2025-03-07 19:14:00 +0545

    structure for v3 helping bot

---
+++ Bot_v3/ingestion.py
@@ -0,0 +1,10 @@
+from langchain_community.document_loaders import ReadTheDocsLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+def ingest_docs() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    ingest_docs()
